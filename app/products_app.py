a1f58dec58e25cdfca11ca52a3e0b752e83fc5a3
Brian Guirnalda <Brian.guirnalda@stern.nyu.edu>
2017-07-23 17:04:19 -0400

    updated menu and listing products

--- app/products_app.py
+++ app/products_app.py
@@ -9,33 +9,53 @@ with open(csv_file_path, "r") as csv_file:
     for row in reader:
         products.append (row)
 
-print(len (products))
-
 menu = """
 
-    Hi.
+------------------------------------
+BRIAN'S AWESOME PRODUCTS APPLICATION
+------------------------------------
+Hello and welcome {0},
 
-    Welcome to the products app.
+There are {1} products in the database. Please select an operation:
 
-    There are 100 products.
+    operation | description
+    --------- | ------------------
+    'List'    | Display a list of product identifiers and names.
+    'Show'    | Show information about a product.
+    'Create'  | Add a new prodcut.
+    'Update'  | Edit an existing product.
+    'Destroy' | Delete an existing product.
 
-    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
 
-    Please choose an operation:
-
-"""
+""".format("Brianguirnalda1", len(products))
 
 chosen_operation = input(menu)
 chosen_operation = chosen_operation.title()
 
 def list_products():
     print("LISTING PRODUCTS")
+    for product in products:
+        print(" + Product #" + str(product["id"]) + ": " + product["name"])
 
 def show_product():
     print("SHOWING A PRODUCT")
 
 def create_product():
     print("CREATING A PRODUCT")
+    product_name = input("name is:")
+    product_aisle = input("aisle is:")
+    product_department = input("department is:")
+    product_price = input("price is:")
+    new_product = {
+        "id": len(products) + 1,
+        "name": product_name,
+        "aisle": product_aisle,
+        "department": product_department,
+        "price": product_price
+}
+    print("NEW PRODUCT IS", new_product)
+    products.append(new_product)
+
 
 def update_product():
     print("UPDATING A PRODUCT")
@@ -43,9 +63,17 @@ def update_product():
 def destroy_product():
     print("DESTROYING A PRODUCT")
 
+
 if chosen_operation == "List": list_products()
 elif chosen_operation == "Show": show_product()
 elif chosen_operation == "Create": create_product()
 elif chosen_operation == "Update": update_product()
 elif chosen_operation == "Destroy": destroy_product()
 else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
+
+
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)
